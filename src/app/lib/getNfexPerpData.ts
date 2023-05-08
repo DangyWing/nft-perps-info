d62e027db1b6fc30c3dcd8afb69e4b3efad0c8d8
DangyWing <dangywing@gmail.com>
2023-05-08 10:48:41 -0400

    actually do the refactor to use the one endpoint instead of looping for amms

---
+++ src/app/lib/getNfexPerpData.ts
@@ -0,0 +1,18 @@
+import { type NfexDataResponse } from "~/types";
+import { parseNfexData } from "~/utils/parseNfexData";
+
+export async function getNfexPerpData() {
+  const res = await fetch("https://apigw.nfex.io/market/pairs", {
+    next: {
+      revalidate: 10,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const { data } = (await res.json()) as NfexDataResponse;
+
+  return parseNfexData(data);
+}
--- src/app/lib/getPerpData.tsx
+++ src/app/lib/getPerpData.tsx
@@ -1,68 +1,11 @@
-import { type NfexDataResponse } from "~/types";
-import { parseNfexData } from "~/utils/parseNfexData";
-import { AMMData } from "~/constants/constants";
-import type { AMMResponse, NftPerpData, PerpData } from "~/types";
+import type { PerpData } from "~/types";
 import { calculate_percentage_change } from "~/utils/utils";
-
-async function getNftPerpData() {
-  const nftPerpData = [];
-  for (const amm of AMMData) {
-    const url = `https://api3.nftperp.xyz/${amm.AMM}`;
-    const res = await fetch(url, {
-      next: {
-        revalidate: 30,
-      },
-    });
-
-    const { data } = (await res.json()) as AMMResponse;
-
-    const funding =
-      Number(data.nextEstimatedFundingRateLong) > 0
-        ? Number(data.nextEstimatedFundingRateShort) * 100
-        : Number(data.nextEstimatedFundingRateLong) * 100;
-
-    const fundingSide =
-      parseFloat(data.nextEstimatedFundingRateLong) > 0 ? "Short" : "Long";
-
-    const indexToMarkDelta =
-      (Math.abs(parseFloat(data.indexPrice) - parseFloat(data.markPrice)) /
-        ((parseFloat(data.indexPrice) + parseFloat(data.markPrice)) / 2)) *
-      100;
-
-    const perpData: NftPerpData = {
-      projectName: amm.label,
-      nftPerpIndexPrice: data.indexPrice.toString(),
-      nftPerpMarkPrice: data.markPrice.toString(),
-      nftPerpFundingRate: funding.toString(),
-      nftPerpIndexToMark: indexToMarkDelta,
-      nftPerpSlug: amm.AMM,
-      nftPerpFundingSide: fundingSide,
-    };
-
-    nftPerpData.push(perpData);
-  }
-  return nftPerpData;
-}
-
-async function getNfexData() {
-  const res = await fetch("https://apigw.nfex.io/market/pairs", {
-    next: {
-      revalidate: 10,
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const { data } = (await res.json()) as NfexDataResponse;
-
-  return parseNfexData(data);
-}
+import { getNftPerpData } from "./getNftPerpData";
+import { getNfexPerpData } from "./getNfexPerpData";
 
 export async function getPerpData() {
   const [nfexData, nftPerpData] = await Promise.all([
-    await getNfexData(),
+    await getNfexPerpData(),
     await getNftPerpData(),
   ]);
 
