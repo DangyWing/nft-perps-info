39db853418233bb65efa03ae47dc9b876209581e
DangyWing <dangywing@gmail.com>
2023-05-16 16:12:30 -0400

    added multicall to the public rpc

--- src/utils/viemClient.ts
+++ src/utils/viemClient.ts
@@ -3,6 +3,12 @@ import { arbitrum } from "viem/chains";
 
 export const publicClient = createPublicClient({
   chain: arbitrum,
+  batch: {
+    multicall: {
+      batchSize: 1_024,
+      wait: 16,
+    },
+  },
   transport: http(
     "https://arbitrum-mainnet.infura.io/v3/cedae0e5449f4711a498a87ba69b133e"
   ),
