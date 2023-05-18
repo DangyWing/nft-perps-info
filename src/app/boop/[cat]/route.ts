245bd158df4be8738e57c1ddbda8e6ace9c40e10
DangyWing <dangywing@gmail.com>
2023-05-18 10:34:53 -0400

    test route

---
+++ src/app/boop/[cat]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function GET(
+  _request: Request,
+  {
+    params,
+  }: {
+    params: {
+      cat: string;
+    };
+  }
+) {
+  // console.log(_request);
+  // console.log(params);
+  const res = {
+    foo: "bar",
+    boop: params.cat,
+  };
+
+  return NextResponse.json(res);
+}
