fca4bbcb32a76f75a53deb249653696a4e8fad82
DangyWing <dangywing@gmail.com>
2023-05-18 10:56:24 -0400

    route with search params

---
+++ src/app/boop/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function GET(request: Request) {
+  // console.log(request);
+  const { searchParams } = new URL(request.url);
+
+  // console.log(searchParams);
+
+  const cat = searchParams.get("cat");
+
+  const res = {
+    foo: "bar",
+    boop: cat,
+  };
+
+  return NextResponse.json(res);
+}
