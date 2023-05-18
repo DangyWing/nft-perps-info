f311fde63faac5a790d323ea67de0bd24f0679bf
DangyWing <dangywing@gmail.com>
2023-05-18 10:59:47 -0400

    simplify boop route

--- src/app/boop/route.ts
+++ src/app/boop/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 
 export function GET(request: Request) {
-  // console.log(request);
-  const { searchParams } = new URL(request.url);
+  console.log(request.url);
+  // const { searchParams } = new URL(request.url);
 
   // console.log(searchParams);
 
-  const cat = searchParams.get("cat");
+  // const cat = searchParams.get("cat");
 
   const res = {
     foo: "bar",
-    boop: cat,
+    // boop: cat,
   };
 
   return NextResponse.json(res);
